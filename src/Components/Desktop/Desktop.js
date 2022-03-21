67a09a966e39037a51f514c981880f59a40663f9
Andrey <priv1984@yandex.ru>
2022-03-21 23:51:56 +0700

    Create

---
+++ src/Components/Desktop/Desktop.js
@@ -0,0 +1,25 @@
+import React from "react";
+import style from './Desktop.module.css'
+
+const Desktop = () => {
+    return <>
+        <div className={style.desktop}>
+            <div className={style.card}>
+                Hi!
+            </div>
+            <div className={style.card}>
+                Hi!
+            </div>
+            <div className={style.card}>
+                Hi!
+            </div>
+            <div className={style.card}>
+                Hi!
+            </div>
+
+        </div>
+    </>
+
+}
+
+export default Desktop;
