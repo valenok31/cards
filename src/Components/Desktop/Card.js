f6bf2c527d9e4b5812aaaf509cb75851bd06e759
Andrey <priv1984@yandex.ru>
2022-03-28 20:09:18 +0700

    implementing the Formik library

--- src/Components/Desktop/Card.js
+++ src/Components/Desktop/Card.js
@@ -28,59 +28,46 @@ const Card = (props) => {
         },
     });
 
-    let inputName = <input
-        size='7'
-        id="name"
-        name="name"
-        type="text"
-        autoFocus={true}
-        onChange={formikName.handleChange}
-        value={formikName.values.name}
-        onBlur={formikName.handleSubmit}
-    />
-    let inputField = <input
-        size='7'
-        id="translation_ru"
-        name="translation_ru"
-        type="text"
-        autoFocus={true}
-        onChange={formikTranslation_ru.handleChange}
-        value={formikTranslation_ru.values.translation_ru}
-        onBlur={formikTranslation_ru.handleSubmit}
-    />
+    let inputName = (
+        <form onSubmit={formikName.handleSubmit}>
+            <input
+                size='7'
+                id="name"
+                name="name"
+                type="text"
+                autoFocus={true}
+                onChange={formikName.handleChange}
+                value={formikName.values.name}
+                onBlur={formikName.handleSubmit}
+            />
+        </form>)
 
-    const InputForm = (props) => {
-        return <>
-            <form onSubmit={props.formik.handleSubmit}>
-                <input
-                    size='7'
-                    id={props.id}
-                    name={props.id}
-                    type="text"
-                    autoFocus={true}
-                    onChange={props.formik.handleChange}
-                    value={props.formik.values.value}
-                    onBlur={props.formik.handleSubmit}
-                />
-            </form>
-        </>
-    }
-
+    let inputField = (
+        <form onSubmit={formikTranslation_ru.handleSubmit}>
+            <input
+                size='7'
+                id="translation_ru"
+                name="translation_ru"
+                type="text"
+                autoFocus={true}
+                onChange={formikTranslation_ru.handleChange}
+                value={formikTranslation_ru.values.translation_ru}
+                onBlur={formikTranslation_ru.handleSubmit}
+            />
+        </form>)
     /* end Formik */
 
     return <>
-        <InputForm id={'translation_ru'} formik={formikTranslation_ru}/>
         <div className={style.card}>
             <div className={style.card__delete} onClick={() => props.addCardDelete(newName)}>X</div>
             <div className={style.card__name}>
-                {switchCardName ? cardName : <form onSubmit={formikName.handleSubmit}>{inputName}</form>}
+                {switchCardName ? cardName : inputName}
             </div>
             <div className={style.card__transcription}>
                 {cardTranscription}
             </div>
             <div className={style.card__field}>
-                {switchCardField ? cardField : <form onSubmit={formikTranslation_ru.handleSubmit}>{inputField}</form>}
-                {/*<input value={props.opt.translation_ru} size='5' autoFocus={true} onBlur={() => props.editFieldCard(newName)}/>*/}
+                {switchCardField ? cardField : inputField}
             </div>
         </div>
     </>
